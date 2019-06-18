ddb1c11b5553633470308a3a8f0530487742c814
Diego Gomes Araujo <diego.gomes@tokiomarine.com.br>
2019-06-18 14:15:29 -0300

    start basic classification case from tensorflow tutorial

---
+++ example_tutorials/basic_classification.py
@@ -0,0 +1,20 @@
+
+# tutorial url: https://www.tensorflow.org/tutorials/keras/basic_classification
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
