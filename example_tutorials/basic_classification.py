459e100030b2bbf6d8a30762d67a3ca6756cc148
Diego <diego@diego.com>
2019-06-20 22:01:51 -0300

    text classification example

--- example_tutorials/basic_classification.py
+++ example_tutorials/basic_classification.py
@@ -50,3 +50,72 @@ print(predictions[0])
 maxConfidence = np.argmax(predictions[0])
 
 print('Type: ', class_names[maxConfidence])
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap = plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    label = "{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                        100 * np.max(predictions_array),
+                                        class_names[true_label],
+                                        color = color)
+    plt.xlabel(label)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    thisplot = plt.bar(range(10), predictions_array, color = '#777777')
+    plt.ylim([0, 1])
+
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+#num_rows = 5
+#num_cols = 3
+#num_images = num_rows * num_cols
+
+#plt.figure(figsize = (2 * 2 * num_cols, 2 * num_rows))
+#for i in range(num_images):
+#    plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+#    plot_image(i, predictions, test_labels, test_images)
+#    plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+#    plot_value_array(i, predictions, test_labels)
+
+#plt.show()
+
+img = test_images[153]
+
+print(img.shape)
+
+img = np.expand_dims(img, 0)
+
+print(img.shape)
+
+predictions_single = model.predict(img)
+
+print(predictions_single)
+
+plot_value_array(0, predictions_single, test_labels)
+_ = plt.xticks(range(10), class_names, rotation = 45)
+plt.show()
+
+print(np.argmax(predictions_single[0]))
